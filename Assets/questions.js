d315d05491f26d627424d6c849a5005150f86d53
brandon chontos <brandonchontos@gmail.com>
2020-03-15 20:06:56 -0400

    Homework4

---
+++ Assets/questions.js
@@ -0,0 +1,41 @@
+var questions = [{
+    question: "What does CSS stand for?",
+    option1: "Compact Style Sheet",
+    option2: "Computer Screen Sheet",
+    option3: "Complex Style Sheet",
+    option4: "Cascading Style Sheet",
+    answer: "3"
+  },
+  {
+    question: "The  ____ position property is used to define the initial position of the background image.",
+    option1: "background",
+    option2: "margin",
+    option3: "style",
+    option4: "front",
+    answer: "0"
+  },
+  {
+    question: "Which HTML tag us used to define an internal style sheet",
+    option1: "<style>",
+    option2: "<css>",
+    option3: "<script>",
+    option4: "<js>",
+    answer: "0"
+  },
+  {
+    question: "Which HTML attribute is used to define inline styles?",
+    option1: "font",
+    option2: "class",
+    option3: "styles",
+    option4: "style",
+    answer: "3"
+  },
+  {
+    question: "Javascript Code can be called by using",
+    option1: "RMI",
+    option2: "Triggering Event",
+    option3: "PrepProcessor",
+    option4: "Function/Method",
+    answer: "3"
+  },
+];
